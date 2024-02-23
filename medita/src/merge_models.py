0cecdc39960f7123f03b027ce9de27e348bca372
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2024-02-23 15:22:43 +0100

    add directoyr

--- medita/src/merge_models.py
+++ medita/src/merge_models.py
@@ -2,8 +2,9 @@ import os
 import sys
 
 dir = 'medita-model'
+config_file = 'medita_merge.yaml'
 if not os.path.exists(dir):
     os.makedirs(dir)
-cli = f"mergekit-yaml config.yaml {dir} --copy-tokenizer --allow-crimes --out-shard-size 1B"
+cli = f"mergekit-yaml {config_file} {dir} --copy-tokenizer --allow-crimes --out-shard-size 1B"
 # Now I run the command
 os.system(cli)
